c3cbb4256af8fb5168dc3457a215fbdb275bf65a
AmirHossein <41341017+amirhosseinrahmati@users.noreply.github.com>
2018-11-24 14:12:16 +0330

    Update ProfileItem.js

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -18,7 +18,7 @@ class ProfileItem extends Component {
             <p>
               {profile.status}
               {isEmpty(profile.company) ? null : (
-                <span>at {profile.company}</span>
+                <span> at {profile.company}</span>
               )}
             </p>
             <p>
